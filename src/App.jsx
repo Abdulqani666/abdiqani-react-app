3063b218dc6041dc2dcd4c2b931c3b6dfe47f62e
abdiqani <abdiqanimuse124@gmail.com>
2025-05-17 06:46:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import Abdi from './components/Abdi'
+
+function App() {
+  return (
+    <>
+    <Abdi name="abdiqani" age="24" country="somalia"/>
+   <Header />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Abdi.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Abdi(props) {
+  return (
+    <div>
+      <h1>this is my first react app</h1>
+      <h2>{props.name}</h2>
+      <h3>{props.age}</h3>
+      <h4>{props.country}</h4>
+    </div>
+  )
+}
+
+
+export default Abdi
