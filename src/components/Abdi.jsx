780b123c29160f2d3fef1af6e261ced68f632c41
abdiqani <abdiqanimuse124@gmail.com>
2025-05-17 08:12:29 +0300

    I have added a h1 line

--- src/components/Abdi.jsx
+++ src/components/Abdi.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function Abdi(props) {
   return (
-    <div>
+    <Fragment>
       <h1>this is my first react app</h1>
       <h2>{props.name}</h2>
       <h3>{props.age}</h3>
       <h4>{props.country}</h4>
-    </div>
+      <h1>this is my second commit </h1>
+    </Fragment>
   )
 }
 
